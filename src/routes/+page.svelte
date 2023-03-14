520eafde03ea768ceaf5b4bc735d236b27dfe7f4
TeePuppet <silviu@silviu.co.uk>
2023-03-14 16:48:56 +0200

    fix alignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen">
+<div id="logo-container" class="mt-5 flex flex-col justify-start h-screen px-4 box-border sm:mt-0 sm:justify-center sm:items-center">
+    <h1 class="text-2xl pb-4 font-bold">Login</h1>
 	<EmailAndPasswordLogin />
 </div>
-
