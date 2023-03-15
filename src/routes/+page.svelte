4e397fd07fa9b361be88130453fff009cab3a3b6
TeePuppet <silviu@silviu.co.uk>
2023-03-15 14:49:22 +0200

    fix css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,6 @@
 
 </script>
 
-<div id="logo-container" class="mt-5 flex flex-col justify-start h-screen px-4 box-border sm:mt-0 sm:justify-center sm:items-center">
-    <h1 class="text-2xl pb-4 font-bold">Login</h1>
+<div class="h-full py-6 px-6 flex flex-col justify-start box-border sm:justify-center sm:items-center">
 	<EmailAndPasswordLogin />
 </div>
