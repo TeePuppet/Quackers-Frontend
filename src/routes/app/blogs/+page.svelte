c9ab31f7ef8673f6a74da61ad49a138de956b4df
TeePuppet <silviu@silviu.co.uk>
2023-03-26 11:36:01 +0300

    test inputs

--- src/routes/app/blogs/+page.svelte
+++ src/routes/app/blogs/+page.svelte
@@ -8,13 +8,8 @@
         <Modal>
             <h1 slot="header">Adauga un site nou</h1>
             <div slot="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar etiam non quam lacus. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat. Urna porttitor rhoncus dolor purus non. Amet tellus cras adipiscing enim eu turpis egestas pretium aenean. Odio aenean sed adipiscing diam donec. Accumsan in nisl nisi scelerisque eu. Quis enim lobortis scelerisque fermentum dui faucibus in. Imperdiet dui accumsan sit amet nulla facilisi. Nisl rhoncus mattis rhoncus urna neque viverra justo nec. Cursus turpis massa tincidunt dui.
-
-Semper risus in hendrerit gravida rutrum quisque. Integer feugiat scelerisque varius morbi enim nunc. In egestas erat imperdiet sed euismod. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Eget nunc scelerisque viverra mauris in. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Ullamcorper a lacus vestibulum sed arcu. Imperdiet dui accumsan sit amet nulla facilisi. Imperdiet massa tincidunt nunc pulvinar. Quisque id diam vel quam elementum pulvinar. Sed risus ultricies tristique nulla aliquet enim tortor. Quis vel eros donec ac odio tempor orci dapibus.
-
-Enim diam vulputate ut pharetra. Ultrices eros in cursus turpis. Dolor sit amet consectetur adipiscing elit ut aliquam. Elementum curabitur vitae nunc sed. In massa tempor nec feugiat nisl pretium fusce id. Pharetra et ultrices neque ornare aenean euismod. Ultricies leo integer malesuada nunc vel risus. Diam vel quam elementum pulvinar etiam non quam. Consequat mauris nunc congue nisi vitae suscipit tellus. Nibh nisl condimentum id venenatis a condimentum vitae. Placerat vestibulum lectus mauris ultrices eros in cursus. Non arcu risus quis varius quam quisque id diam vel. Cras tincidunt lobortis feugiat vivamus at augue eget. Neque egestas congue quisque egestas.
-
-Purus in mollis nunc sed id. Odio pellentesque diam volutpat commodo. Mauris rhoncus aenean vel elit scelerisque. Purus viverra accumsan in nisl. Eget lorem dolor sed viverra ipsum. Neque ornare aenean euismod elementum nisi quis. Mauris pellentesque pulvinar pellentesque habitant. Enim sit amet venenatis urna. Scelerisque in dictum non consectetur a erat nam at. Donec pretium vulputate sapien nec sagittis aliquam malesuada. Morbi tempus iaculis urna id volutpat lacus laoreet non. Netus et malesuada fames ac turpis. Bibendum neque egestas congue quisque egestas diam in arcu.
+            <input class="w-full" placeholder="Nume site">
+            <input class="w-full" placeholder="URL Github Repo">
             </div>
         </Modal>
 	</div>
