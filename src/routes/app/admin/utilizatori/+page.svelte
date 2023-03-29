49321e2ed12f0e76f56fac204405e68581568c00
TeePuppet <silviu@silviu.co.uk>
2023-03-29 13:25:35 +0300

    fix missing component

--- src/routes/app/admin/utilizatori/+page.svelte
+++ src/routes/app/admin/utilizatori/+page.svelte
@@ -1,10 +1,7 @@
 <script>
-	import ContentTitle from "$lib/components/ContentTitle.svelte";
 	import PageLayout from "$lib/components/layout/PageLayout.svelte";
 	import Modal from "$lib/components/Modal.svelte";
-	import TopBar from "$lib/components/TopBar.svelte";
 	import UserList from "./UserList.svelte";
-
 	export let data;
 
 </script>
