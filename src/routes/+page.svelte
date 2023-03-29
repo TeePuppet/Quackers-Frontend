f5f7f3255bf7992220b386c3778b6aedca48b91a
TeePuppet <silviu@silviu.co.uk>
2023-03-29 07:48:49 +0300

    fix 500 error + other tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
 	import EmailAndPasswordLogin from "$lib/firebase/components/EmailAndPasswordLogin.svelte";
-	import GoogleAuthLogin from "$lib/firebase/components/GoogleAuthLogin.svelte";
-
-
 </script>
 
 <div class="h-full py-6 px-6 flex flex-col justify-start box-border sm:justify-center sm:items-center">
