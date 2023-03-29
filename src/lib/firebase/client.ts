688a354034dc1488ae415154500bf656dfbe93e6
TeePuppet <silviu@silviu.co.uk>
2023-03-29 21:06:56 +0300

    added firestore to client

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -3,6 +3,7 @@ import { initializeApp } from 'firebase/app';
 // import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
 import { PUBLIC_FIREBASE_API_KEY, PUBLIC_FIREBASE_AUTH_DOMAIN, PUBLIC_FIREBASE_PROJECT_ID, PUBLIC_FIREBASE_STORAGE_BUCKET, PUBLIC_FIREBASE_MESSAGING_SENDER_ID, PUBLIC_FIREBASE_APP_ID, PUBLIC_FIREBASE_MEASUREMENT_ID } from "$env/static/public";
+import { getFirestore } from 'firebase/firestore';
 
 
 // firebase config
@@ -16,10 +17,7 @@ const firebaseConfig = {
     measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
-// export const initFirebase = memoize(() => {
-    const app = initializeApp(firebaseConfig);
-    // export const analytics = getAnalytics(app);
-    export const auth = getAuth(app);
-    // return { app, auth }
-// })
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
+export const auth = getAuth(app);
+
