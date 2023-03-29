625b2ee42065d8623026d9210252cd9457f7749f
TeePuppet <silviu@silviu.co.uk>
2023-03-29 20:49:44 +0300

    fix firebaseInit

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,8 +1,7 @@
 import { PRIVATE_FIREBASE_ADMIN_CLIENT_EMAIL, PRIVATE_FIREBASE_ADMIN_PRIVATE_KEY } from "$env/static/private";
 import { PUBLIC_FIREBASE_PROJECT_ID } from "$env/static/public";
-import { apps, auth } from "firebase-admin";
-import { getApps, getApp, initializeApp, cert } from "firebase-admin/app";
-import type { DecodedIdToken } from "firebase-admin/auth";
+import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
+import { getAuth, type DecodedIdToken } from "firebase-admin/auth";
 
 const projectId = PUBLIC_FIREBASE_PROJECT_ID;
 const clientEmail = PRIVATE_FIREBASE_ADMIN_CLIENT_EMAIL;
@@ -17,17 +16,15 @@ const adminConfig = {
 };
 
 
-const initializeFirebase = () => {
-    if (!apps.length) { initializeApp(adminConfig); }
-}
+export const getAdminApp = () => getApps().length ? getApp() : initializeApp(adminConfig);
 
 export async function decodeToken(token: string): Promise<DecodedIdToken | null> {
     if (!token) return null;
 
     try {
-        initializeFirebase();
+        const auth = getAuth(getAdminApp());
+        return await auth.verifyIdToken(token);
 
-        return await auth().verifyIdToken(token);
     } catch (err) {
         console.error('An error occurred validating token', (err as Error).message);
         return null;
