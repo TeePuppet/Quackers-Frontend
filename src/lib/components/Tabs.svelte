7bed903a13b68cc042b6e7aff9f86e1e58604f11
TeePuppet <silviu@silviu.co.uk>
2023-04-12 10:36:18 +0300

    green selected button

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -19,13 +19,13 @@
 
 <style lang="postcss">
     .tabs ul {
-        @apply mt-4 flex text-sm flex-nowrap gap-2 overflow-x-auto snap-x snap-mandatory py-2;
+        @apply mt-4 flex text-sm flex-nowrap gap-1 overflow-x-auto snap-x snap-mandatory py-2;
     }
 
     .tabs li {
         @apply border border-zinc-900 text-center px-3 py-2 hover:cursor-pointer flex-none rounded-md snap-always snap-start;
     }
     .active {
-        @apply bg-zinc-900;
+        @apply bg-emerald-600;
     }
 </style>
