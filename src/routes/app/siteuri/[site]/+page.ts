e10331a2765466df2e3f1ea0cc17edaf19d42756
TeePuppet <silviu@silviu.co.uk>
2023-04-12 12:28:07 +0300

    update

---
+++ src/routes/app/siteuri/[site]/+page.ts
@@ -0,0 +1,5 @@
+import { getWebsite, Websites, type Website } from '$lib/stores/websites'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+export const load = (async () => {    
+})
