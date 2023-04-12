372fc251a0d2b3f2f2035a7712558e9cd4a338e5
TeePuppet <silviu@silviu.co.uk>
2023-04-12 11:49:08 +0300

    added wake lock

---
+++ src/lib/utils/wakeLock.ts
@@ -0,0 +1,26 @@
+// src/lib/wakeLock.ts
+
+export async function requestWakeLock(): Promise<void> {
+    try {
+      if ('wakeLock' in navigator && 'request' in navigator.wakeLock) {
+        const wakeLock = await navigator.wakeLock.request('screen');
+  
+        wakeLock.addEventListener('release', () => {
+          console.log('Wake Lock was released');
+        });
+  
+        document.addEventListener('visibilitychange', () => {
+          if (document.visibilityState === 'hidden') {
+            wakeLock.release();
+          }
+        });
+  
+        console.log('Wake Lock is active');
+      } else {
+        console.log('Wake Lock API is not supported');
+      }
+    } catch (error:any) {
+      console.error(`Failed to acquire wake lock: ${error.name}, ${error.message}`);
+    }
+  }
+  
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
     import "../app.css";
+    import { onMount } from 'svelte';
+	import { requestWakeLock } from "$lib/utils/wakeLock";
+
+  onMount(async () => {
+    if (typeof window !== 'undefined') {
+      await requestWakeLock();
+    }
+  });
 </script>
 
 
