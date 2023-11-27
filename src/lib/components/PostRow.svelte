85217b9efd89b786e64739c7297ebffd2c0cb8f4
TeePuppet <silviu@silviu.co.uk>
2023-11-27 16:49:27 +0200

    update

--- src/lib/components/PostRow.svelte
+++ src/lib/components/PostRow.svelte
@@ -4,9 +4,26 @@
 <div class="bg-zinc-900 flex gap-2 justify-between px-4 py-2 mb-2 items-center rounded-lg border border-zinc-800">
     <h3>Titlu</h3>
     <p>12 Jan 2023</p>
+    <p><span class="pill ai-draft">AI Draft</span></p>
     <ul class="flex">
         <li>Frigider</li>
         <li>Frigider 5l</li>
     </ul>
     <Button>Edit</Button>
 </div>
+
+<style lang="postcss">
+
+    .pill {
+        @apply text-xs uppercase font-semibold px-2 py-1 rounded-full
+    }
+    .published {
+        @apply bg-green-900 
+    }
+
+    .ai-draft {
+        @apply bg-orange-600
+    }
+
+    
+</style>
